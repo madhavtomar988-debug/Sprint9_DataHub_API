7b4734e794b58661af9d3fe4c5b2aca94ac3710d
madhavtomar988-debug <madhavtomar988@gmail.com>
2026-07-21 23:09:28 +0530

    Sprint 9 completed

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+let blogs = [
+  {
+    id: 1,
+    title: "Welcome Blog",
+    content: "This is the first blog."
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Data Hub API");
+});
+
+app.get("/blogs", (req, res) => {
+  res.json(blogs);
+});
+app.get("/blogs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const blog = blogs.find((b) => b.id === id);
+
+  if (!blog) {
+    return res.status(404).json({
+      message: "Blog not found",
+    });
+  }
+
+  res.json(blog);
+});
+app.post("/blogs", (req, res) => {
+  const { title, content } = req.body;
+
+  const newBlog = {
+    id: blogs.length + 1,
+    title,
+    content,
+  };
+
+  blogs.push(newBlog);
+
+  res.status(201).json({
+    message: "Blog created successfully",
+    blog: newBlog,
+  });
+});
+
+app.put("/blogs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, content } = req.body;
+
+  const blog = blogs.find((b) => b.id === id);
+
+  if (!blog) {
+    return res.status(404).json({
+      message: "Blog not found",
+    });
+  }
+
+  blog.title = title;
+  blog.content = content;
+
+  res.json({
+    message: "Blog updated successfully",
+    blog,
+  });
+});
+app.delete("/blogs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const blogIndex = blogs.findIndex((b) => b.id === id);
+
+  if (blogIndex === -1) {
+    return res.status(404).json({
+      message: "Blog not found",
+    });
+  }
+
+  blogs.splice(blogIndex, 1);
+
+  res.json({
+    message: "Blog deleted successfully",
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (
+    email === "admin@example.com" &&
+    password === "123456"
+  ) {
+    return res.json({
+      success: true,
+      message: "Login successful",
+      token: "mock-jwt-token-123456"
+    });
+  }
+
+  res.status(401).json({
+    success: false,
+    message: "Invalid email or password"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
