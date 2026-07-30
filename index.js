bcb5f0253a1fde6b7f395b9e2598fe5db2694e9e
madhavtomar988-debug <madhavtomar988@gmail.com>
2026-07-31 02:22:41 +0530

    Sprint 9 MongoDB integration completed

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use((req, res, next) => {
